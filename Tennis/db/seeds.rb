2f15bf284604f9bd8cc70c85bb72f566945fd790
BiCo26 <bianca.cortes26@gmail.com>
2017-09-12 18:36:00 -0400

    seeded db

--- Tennis/db/seeds.rb
+++ Tennis/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Song.create!(name: 'Hotter than hell', description: 'pop')
+Song.create!(name: 'Gasoline', description: 'pop')
+Song.create!(name: 'Im a gigalo', description: 'hip hop')
+
+puts "#{Song.count} songs seeded!"
+
+Artist.create!(name: 'Dua Lip', song_id: 2)
+Artist.create!(name: 'Halsey', song_id: 3)
+Artist.create!(name: 'Nick Cannon', song_id: 1)
